913030bb44eb778509d1e5f60969dc6c5c26c48e
kb211 <kas_bouwens@hotmail.com>
2016-06-06 17:16:31 +0200

    openwav fix

--- pythonfiles/openWav.py
+++ pythonfiles/openWav.py
@@ -1,11 +1,20 @@
 from __future__ import print_function
 import librosa
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+import numpy as np
+
 
 
 def loadData():
     audiofile = '../data/toy_data_sines_44_1khz.wav'
     y, sr = librosa.load(audiofile, sr=None)
-    y_test = reshape(y[1:44100*50], (50, 44100))
-    y_train = reshape(y[44100*50:], (450, 44100))
-    return y_test, y_train
+    testInterval = 100*44100
+    #trainInterval = y.shape[0]- testInterval
+    
+    y_test = np.reshape(y[0:testInterval], (100, 44100))
+    y_train = np.reshape(y[testInterval:], (900, 44100))
+    print(y_test.shape)
+    print(y_train.shape)
+    return y_test#, y_train
+
+loadData()
