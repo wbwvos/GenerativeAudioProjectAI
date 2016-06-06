c64ba30c64f2a44f04208f3b3122e052e22b170c
kb211 <kas_bouwens@hotmail.com>
2016-06-06 16:35:37 +0200

    openWav

--- pythonfiles/openWav.py
+++ pythonfiles/openWav.py
@@ -6,4 +6,6 @@ import matplotlib.pyplot as plt
 def loadData():
     audiofile = '../data/toy_data_sines_44_1khz.wav'
     y, sr = librosa.load(audiofile, sr=None)
-    return y[1:44100*50], y[44100*50:]
+    y_test = reshape(y[1:44100*50], (50, 44100))
+    y_train = reshape(y[44100*50:], (450, 44100))
+    return y_test, y_train
