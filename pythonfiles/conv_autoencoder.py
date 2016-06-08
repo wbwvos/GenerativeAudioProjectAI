3c5a44f28d12fae737437168edf2dbaa7d2a5d75
Wolf Vos <wolf@mijnlieff.nl>
2016-06-08 15:13:08 +0200

    conv_autoencoder

--- pythonfiles/conv_autoencoder.py
+++ pythonfiles/conv_autoencoder.py
@@ -1,4 +1,4 @@
-from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input, Dense, Convolution1D, AveragePooling1D, UpSampling1D
 from keras.models import Model
 import openWav
 
@@ -6,22 +6,22 @@ x_train, x_test, sr = openWav.loadData()
 
 input_sample = Input(shape=(1, sr))
 
-x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(input_sample)
-x = MaxPooling2D((2, 2), border_mode='valid')(x)
-x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(x)
-x = MaxPooling2D((2, 2), border_mode='valid')(x)
-x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(x)
-encoded = MaxPooling2D((2, 2), border_mode='valid')(x)
+x = Convolution1D(32, 32, border_mode='same', activation="tanh")(input_sample)
+x = AveragePooling1D(pool_length=2, stride=None, border_mode="valid")(x)
+x = Convolution1D(32, 32, border_mode='same', activation="tanh")(input_sample)
+x = AveragePooling1D(pool_length=2, stride=None, border_mode="valid")(x)
+x = Convolution1D(32, 16, border_mode='same', activation="tanh")(input_sample)
+x = AveragePooling1D(pool_length=2, stride=None, border_mode="valid")(x)
+x = Convolution1D(1, 8, border_mode='same', activation="tanh")(input_sample)
+encoded = AveragePooling1D(pool_length=2, stride=None, border_mode="valid")(x)
 
-# at this point the representation is (8, 4, 4) i.e. 128-dimensional
-
-x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(encoded)
-x = UpSampling2D((2, 2))(x)
-x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(x)
-x = UpSampling2D((2, 2))(x)
-x = Convolution2D(16, 3, 3, activation='relu')(x)
-x = UpSampling2D((2, 2))(x)
-decoded = Convolution2D(1, 3, 3, activation='tanh', border_mode='same')(x)
+x = UpSampling1D(length=2)(encoded)
+x = Convolution1D(32, 32, border_mode='same', activation="tanh")(x)
+x = UpSampling1D(length=2)(x)
+x = Convolution1D(32, 32, border_mode='same', activation="tanh")(x)
+x = UpSampling1D(length=2)(x)
+x = Convolution1D(32, 32, border_mode='same', activation="tanh")(x)
+decoded = Convolution1D(1, 32, border_mode='same', activation="tanh")(x)
 
 autoencoder = Model(input_sample, decoded)
 autoencoder.compile(optimizer='adadelta', loss='mean_squared_error')
