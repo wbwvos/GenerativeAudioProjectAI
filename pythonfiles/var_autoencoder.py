235f03d13f0d62fec8cd9759da843e1a7c3a4334
Wolf Vos <wolf@mijnlieff.nl>
2016-06-10 11:12:36 +0200

    var autoencoder test

--- pythonfiles/var_autoencoder.py
+++ pythonfiles/var_autoencoder.py
@@ -8,10 +8,11 @@ from keras.layers import Input, Dense, Lambda
 from keras.models import Model
 from keras import backend as K
 from keras import objectives
-from keras.datasets import mnist
+
+x_train, x_test, sr = loadData()
 
 batch_size = 16
-original_dim = 44100
+original_dim = sr
 latent_dim = 2
 intermediate_dim = 128
 epsilon_std = 0.01
@@ -34,7 +35,7 @@ z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_std])
 
 # we instantiate these layers separately so as to reuse them later
 decoder_h = Dense(intermediate_dim, activation='relu')
-decoder_mean = Dense(original_dim, activation='sigmoid')
+decoder_mean = Dense(original_dim, activation='tanh')
 h_decoded = decoder_h(z)
 x_decoded_mean = decoder_mean(h_decoded)
 
@@ -47,12 +48,10 @@ vae = Model(x, x_decoded_mean)
 vae.compile(optimizer='rmsprop', loss=vae_loss)
 
 # train the VAE on MNIST digits
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+#(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+x_train = np.resize(x_train, (x_train.shape[0], sr, 1)).astype(np.float32)
+x_test = np.resize(x_test, (x_test.shape[0], sr, 1)).astype(np.float32)
 
 vae.fit(x_train, x_train,
         shuffle=True,
@@ -64,34 +63,18 @@ vae.fit(x_train, x_train,
 encoder = Model(x, z_mean)
 
 # display a 2D plot of the digit classes in the latent space
-x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
-plt.figure(figsize=(6, 6))
-plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test)
-plt.colorbar()
-plt.show()
+#x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
 
 # build a digit generator that can sample from the learned distribution
-decoder_input = Input(shape=(latent_dim,))
-_h_decoded = decoder_h(decoder_input)
-_x_decoded_mean = decoder_mean(_h_decoded)
-generator = Model(decoder_input, _x_decoded_mean)
+#decoder_input = Input(shape=(latent_dim,))
+#_h_decoded = decoder_h(decoder_input)
+#_x_decoded_mean = decoder_mean(_h_decoded)
+#generator = Model(decoder_input, _x_decoded_mean)
 
-# display a 2D manifold of the digits
-n = 15  # figure with 15x15 digits
-digit_size = 28
-figure = np.zeros((digit_size * n, digit_size * n))
-# we will sample n points within [-15, 15] standard deviations
-grid_x = np.linspace(-15, 15, n)
-grid_y = np.linspace(-15, 15, n)
 
-for i, yi in enumerate(grid_x):
-    for j, xi in enumerate(grid_y):
-        z_sample = np.array([[xi, yi]]) * epsilon_std
-        x_decoded = generator.predict(z_sample)
-        digit = x_decoded[0].reshape(digit_size, digit_size)
-        figure[i * digit_size: (i + 1) * digit_size,
-               j * digit_size: (j + 1) * digit_size] = digit
-
-plt.figure(figsize=(10, 10))
-plt.imshow(figure)
-plt.show()
+#z_sample = np.array([[xi, yi]]) * epsilon_std
+#x_decoded = generator.predict(z_sample)
+	   
+	   
+	   
+	   
