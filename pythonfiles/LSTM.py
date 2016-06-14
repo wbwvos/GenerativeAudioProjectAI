b022eda23a27fc36aff0366c283fa00aa13169ab
wbwvos/kb211 @ DAS4 Supercluster <wbwvos@gmail.com>
2016-06-14 14:34:28 +0200

    lstm test

--- pythonfiles/LSTM.py
+++ pythonfiles/LSTM.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
 from keras.optimizers import SGD
+from keras.layers.wrappers import TimeDistributed
 import numpy as np
 import openWav
 
@@ -12,15 +13,29 @@ def train(x_train, y_train, x_test, y_test):
     timesteps = 10
     data_dim = len(x_train[0])
     batchsize = 10    
+    num_hidden_dimensions = data_dim/2
+    num_frequency_dimensions = data_dim
+    print(num_hidden_dimensions)
+    print(num_frequency_dimensions)
+    
+    #model = Sequential()
+    #model.add(LSTM(data_dim, return_sequences=False, batch_input_shape=(10, data_dim, 1)))  
+    #model.compile(loss='mean_squared_error', optimizer=SGD(lr=0.01, momentum=0.9))
     
     model = Sequential()
-    model.add(LSTM(data_dim, return_sequences=False, input_shape=(data_dim, 1)))  
-    model.compile(loss='mean_squared_error', optimizer=SGD(lr=0.01, momentum=0.9))
-    
-    print("fitting")
-    model.fit(x_train, y_train, batch_size=10, nb_epoch=5, validation_data=(x_test, y_test))
+    # This layer converts frequency space to hidden space
+    model.add(TimeDistributed(Dense(num_hidden_dimensions), input_shape=(None, num_frequency_dimensions)))
+    model.add(LSTM(num_hidden_dimensions, return_sequences=True, stateful=False))
+
+    # This layer converts hidden space back to frequency space
+    model.add(TimeDistributed(Dense(input_dim=num_hidden_dimensions, output_dim=num_frequency_dimensions)))
+    model.compile(loss='mean_squared_error', optimizer='rmsprop')
+    model.summary()
+    exit
+    #print("fitting")
+    #model.fit(x_train, y_train, batch_size=10, nb_epoch=5, validation_data=(x_test, y_test))
+    model.fit(x_train, y_train, batch_size=10, nb_epoch=5, verbose=1, validation_split=0.0)
     model.save_weights("weights_RNN.dat", True)
-    
     return model
     
 def predict():
