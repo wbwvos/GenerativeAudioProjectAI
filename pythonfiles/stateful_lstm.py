ac5f6804f61ed83789e7a9f89fe768218c0d4828
Wolf Vos <wolf@mijnlieff.nl>
2016-06-17 13:06:16 +0200

    stateful met opslaan

--- pythonfiles/stateful_lstm.py
+++ pythonfiles/stateful_lstm.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 import openWav
+import time
+import os.path
 
 # since we are using stateful rnn tsteps can be set to 1
 tsteps = 1
@@ -14,8 +16,9 @@ batch_size = 80
 epochs = 10
 # number of elements ahead that are used to make the prediction
 lahead = 1
-
-x_train, y_train, x_test, y_test, sr = openWav.lstmDataStream('../data/drums/128bpm hip1.wav')
+audiodir = '../data/drums/'
+audiofile = '128bpm hip1.wav'
+x_train, y_train, x_test, y_test, sr = openWav.lstmDataStream(audiodir + audiofile)
 
 #def gen_cosine_amp(amp=100, period=25, x0=0, xn=50000, step=1, k=0.0001):
 #    """Generates an absolute cosine time series with the amplitude
@@ -61,41 +64,36 @@ model.add(LSTM(50,
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='rmsprop')
 
-print('Training')
-for i in range(epochs):
-    print('Epoch', i, '/', epochs)
-    model.fit(x_train,
-              y_train,
-              batch_size=batch_size,
-              verbose=1,
-              nb_epoch=1,
-              shuffle=False)
-    model.reset_states()
+weights_filename = 'weights_stateful_lstm.dat'
+if os.path.isfile(weights_filename):
+    print('Loading the model...')
+    model.load_weights(weights_filename)
+else:
+    print('Training the model...')
+    trainStart = time.time()
+    for i in range(epochs):
+        print('Epoch', i, '/', epochs)
+        model.fit(x_train,
+                  y_train,
+                  batch_size=batch_size,
+                  verbose=1,
+                  nb_epoch=1,
+                  shuffle=False)
+        model.reset_states()
+    trainEnd = time.time()
+    print('Trained the model in', trainEnd - trainStart, 'seconds')
+    print('Saving the model...')
+    model.save_weights(weights_filename, True)
 
 print('Predicting')
 predicted_output = model.predict(x_test, batch_size=batch_size)
 
-weights_filename = 'weights_conv.dat'
-    if os.path.isfile(weights_filename):
-        print 'Loading the model...'
-        autoencoder.load_weights(weights_filename)
-    else:
-        print 'Training the model...'
-        trainStart = time.time()
-        autoencoder.fit(x_train, x_train,
-                    nb_epoch=500,
-                    batch_size=10,
-                    shuffle=True,
-                    validation_data=(x_test, x_test))
-        trainEnd = time.time()
-        print 'Trained the model in', trainEnd - trainStart, 'seconds'
-        print 'Saving the model...'
-        model.save_weights(weights_filename, True)
-
 import pickle 
 expected_output = y_test
-pickle.dump(predicted_output, open('predicted.p','wb'))
-pickle.dump(expected_output, open('expected.p','wb'))
+pickle.dump(predicted_output, open('predicted_' + audiofile + '.p','wb'))
+print('Saved predicted_output to predicted.p')
+pickle.dump(expected_output, open('expected_' + audiofile + '.p','wb'))
+print('Saved expected_output to expected.p')
 #
 #print('Ploting Results')
 #plt.subplot(2, 1, 1)
