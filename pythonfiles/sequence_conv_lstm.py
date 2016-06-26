4c1acce725de50ce4d6c22eb2662b7f8d3835a40
Wolf Vos <wolf@mijnlieff.nl>
2016-06-26 14:23:59 +0200

    sequence_conv_lstm aangepast

--- pythonfiles/sequence_conv_lstm.py
+++ pythonfiles/sequence_conv_lstm.py
@@ -10,114 +10,119 @@ import openWav
 import time
 import os.path
 import autoencoders as ae
-# since we are using stateful rnn tsteps can be set to 1
-tsteps = 1
-batch_size = 1
-epochs = 5
-dim = 32
-# number of elements ahead that are used to make the prediction
-sr = 44100
-conv_pack_input_size = 512
-nb_slices = 2
-x_train_e, y_train_e = openWav.loadDrumsConv(nb_slices, conv_pack_input_size, sr)
-encoder, decoder = ae.getSplitConvAutoEncoder()
-#x_train_e = openWav.encodeDrums(x_train, encoder)
-#y_train_e = openWav.encodeDrums(y_train, encoder)
-#x_train = x_train[0]
-#y_train = y_train[0]
-
-max_timesteps = (x_train_e.shape[0]/batch_size)*batch_size
-x_train_e = x_train_e[:max_timesteps]
-y_train_e = np.reshape(y_train_e[:max_timesteps], (max_timesteps, dim))
-print(x_train_e.shape)
-print(y_train_e.shape)
-print('Creating Model')
-model = Sequential()
-model.add(LSTM(128,
-               batch_input_shape=(batch_size, tsteps, dim*nb_slices),
-               return_sequences=True,
-               stateful=True))
-model.add(LSTM(128,
-               #batch_input_shape=(batch_size, tsteps, dim),
-               return_sequences=False,
-               stateful=True))
-model.add(Dense(32))
-model.compile(loss='mse', optimizer='rmsprop')
-model.summary()
-
-weights_filename = 'weights_conv_sequence_lstm_mp_' + str(nb_slices) + 'slices.dat'
-if os.path.isfile(weights_filename):
-    print('Loading the model...')
-    model.load_weights(weights_filename)
-else:
-    print('Training the model...')
-    trainStart = time.time()
-    for i in range(epochs):
-        print('Epoch', i+1, '/', epochs)
-        model.fit(x_train_e,
-                  y_train_e,
-                  batch_size=batch_size,
-                  verbose=1,
-                  nb_epoch=1,
-                  shuffle=False)
-        model.reset_states()
-    trainEnd = time.time()
-    print('Trained the model in', trainEnd - trainStart, 'seconds')
-    print('Saving the model...')
-    model.save_weights(weights_filename, True)
-
-
-
-print('Predicting')
-prime_secs = 10
-gen_secs = 10
-samples_per_sec = 86
-prime = x_train_e[:batch_size*samples_per_sec*prime_secs*nb_slices]
-print('prime time:', batch_size*samples_per_sec*prime_secs*16)
-print('prime shape:', prime.shape)
-generations = batch_size*samples_per_sec*gen_secs
-print('Predicting prime')
-predicted_output = model.predict(prime, batch_size=batch_size, verbose=True)
-print(predicted_output.shape)
-#print(predicted_output)
-#print(len(predicted_output))
-total = np.reshape(prime, (prime.shape[0], prime.shape[1]))
-total = total[:,:dim]
-print('totaldim', total.shape)
-print('total generations:', generations)
-for i in range(generations):
-    #print('totalshape:', total.shape)
-    last_batch = total[-batch_size*nb_slices:,:]
-    #print('last batch shape:', last_batch.shape)
-    last_batch = np.reshape(last_batch, (batch_size, dim*nb_slices, 1))
-    #print('last batch shape:', last_batch.shape)
-    predicted_output_batch = model.predict(last_batch, batch_size=batch_size, verbose=True)
-    predicted_value = predicted_output_batch[-1]
-    #print('total:', total.shape, 'predicted_value:', predicted_value.shape)
-    total = np.vstack([total, predicted_value])
-    #if i % 64 == 0:
-    #print(i, 'predicted:', predicted_value)
-    #model.reset_states()
-    #print(total[-10:])
-print('done generating')     
-
-#print('total.shape:', total.shape)
-
-
-total = np.reshape(total, (total.shape[0], total.shape[1] , 1))
-#print('total.shape', total.shape)
-sound = openWav.decodeDrums(total, decoder)
-
-print(sound.shape)
-
 import pickle 
-#expected_output = y_train
-#pickle.dump(predicted_output, open('predicted.p','wb'))
-#print('Saved predicted_output to predicted.p')
-#pickle.dump(prime, open('expected.p','wb'))
-#print('Saved expected_output to expected.p')
-pickle.dump(sound, open('generated_sound_' + str(nb_slices) +'.p','wb'))
-print('Saved generated_output to generated_sound.p')
+# since we are using stateful rnn tsteps can be set to 1
+
+for nb_slices in [1,2,3,5,8,10,20,40,80]:
+    print('==============================')
+    print('nb_slices', nb_slices)
+    tsteps = 1
+    batch_size = 1
+    epochs = 5
+    dim = 32
+    # number of elements ahead that are used to make the prediction
+    sr = 44100
+    conv_pack_input_size = 512
+    #nb_slices = 2
+    x_train_e, y_train_e = openWav.loadDrumsConv(nb_slices, conv_pack_input_size, sr)
+    encoder, decoder = ae.getSplitConvAutoEncoder()
+    #x_train_e = openWav.encodeDrums(x_train, encoder)
+    #y_train_e = openWav.encodeDrums(y_train, encoder)
+    #x_train = x_train[0]
+    #y_train = y_train[0]
+    
+    max_timesteps = (x_train_e.shape[0]/batch_size)*batch_size
+    x_train_e = x_train_e[:max_timesteps]
+    y_train_e = np.reshape(y_train_e[:max_timesteps], (max_timesteps, dim))
+    #print(x_train_e.shape)
+    #print(y_train_e.shape)
+    print('Creating Model')
+    model = Sequential()
+    model.add(LSTM(128,
+                   batch_input_shape=(batch_size, tsteps, dim*nb_slices),
+                   return_sequences=True,
+                   stateful=True))
+    model.add(LSTM(128,
+                   #batch_input_shape=(batch_size, tsteps, dim),
+                   return_sequences=False,
+                   stateful=True))
+    model.add(Dense(32))
+    model.compile(loss='mse', optimizer='rmsprop')
+    model.summary()
+    
+    weights_filename = 'weights_conv_sequence_lstm_mp_' + str(nb_slices) + 'slices.dat'
+    if os.path.isfile(weights_filename):
+        print('Loading the model...')
+        model.load_weights(weights_filename)
+    else:
+        print('Training the model...')
+        trainStart = time.time()
+        for i in range(epochs):
+            print('Epoch', i+1, '/', epochs)
+            model.fit(x_train_e,
+                      y_train_e,
+                      batch_size=batch_size,
+                      verbose=1,
+                      nb_epoch=1,
+                      shuffle=False)
+            model.reset_states()
+        trainEnd = time.time()
+        print('Trained the model in', trainEnd - trainStart, 'seconds')
+        print('Saving the model...')
+        model.save_weights(weights_filename, True)
+    
+    
+    
+    print('Predicting')
+    prime_secs = 10
+    gen_secs = 10
+    samples_per_sec = 86
+    prime = x_train_e[:batch_size*samples_per_sec*prime_secs*nb_slices]
+    print('prime time:', batch_size*samples_per_sec*prime_secs*16)
+    print('prime shape:', prime.shape)
+    generations = batch_size*samples_per_sec*gen_secs
+    print('Predicting prime')
+    predicted_output = model.predict(prime, batch_size=batch_size)#, verbose=True)
+    print(predicted_output.shape)
+    #print(predicted_output)
+    #print(len(predicted_output))
+    total = np.reshape(prime, (prime.shape[0], prime.shape[1]))
+    total = total[:,:dim]
+    print('totaldim', total.shape)
+    print('total generations:', generations)
+    for i in range(generations):
+        #print('totalshape:', total.shape)
+        last_batch = total[-batch_size*nb_slices:,:]
+        #print('last batch shape:', last_batch.shape)
+        last_batch = np.reshape(last_batch, (batch_size, dim*nb_slices, 1))
+        #print('last batch shape:', last_batch.shape)
+        predicted_output_batch = model.predict(last_batch, batch_size=batch_size)# , verbose=True)
+        predicted_value = predicted_output_batch[-1]
+        #print('total:', total.shape, 'predicted_value:', predicted_value.shape)
+        total = np.vstack([total, predicted_value])
+        #if i % 64 == 0:
+        #print(i, 'predicted:', predicted_value)
+        #model.reset_states()
+        #print(total[-10:])
+    print('done generating')     
+    
+    #print('total.shape:', total.shape)
+    
+    
+    total = np.reshape(total, (total.shape[0], total.shape[1] , 1))
+    #print('total.shape', total.shape)
+    sound = openWav.decodeDrums(total, decoder)
+    
+    #print(sound.shape)
+    
+
+    #expected_output = y_train
+    #pickle.dump(predicted_output, open('predicted.p','wb'))
+    #print('Saved predicted_output to predicted.p')
+    #pickle.dump(prime, open('expected.p','wb'))
+    #print('Saved expected_output to expected.p')
+    pickle.dump(sound, open('generated_sound_' + str(nb_slices) +'.p','wb'))
+    print('Saved generated_output to generated_sound.p')
 
 #
 #print('Ploting Results')
